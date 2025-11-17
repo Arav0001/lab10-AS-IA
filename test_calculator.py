23a09a966765a2f6e41ac3fff4eff525b25694ee
Ismailnotsus <91224837+Ismailnotsus@users.noreply.github.com>
2025-11-16 19:17:24 -0500

    added unittests

--- test_calculator.py
+++ test_calculator.py
@@ -2,13 +2,15 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self):
+        self.assertEqual(3, add(1, 2))
+        self.assertEqual(6, add(5, 1))
+        self.assertEqual(7, add(-3, 10))
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self):
+        self.assertEqual(-6, subtract(2, 8))
+        self.assertEqual(6, subtract(4, -2))
+        self.assertEqual(7, subtract(11, 4))
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -18,21 +20,18 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self):
+        self.assertAlmostEqual(2, logarithm(10, 100))
+        self.assertAlmostEqual(3, logarithm(2, 8))
+        self.assertAlmostEqual(5, logarithm(3, 243))
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
-    
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            logarithm(-4, 64)
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
